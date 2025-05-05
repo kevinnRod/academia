044eda8a931aa9fcf62fb14b77c82e84d87dfb91
kevinnRod <kevinrodp19@gmail.com>
2025-05-05 04:52:12 -0500

    Update: Model docente

--- app/Models/Docente.php
+++ app/Models/Docente.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use MicrosoftAzure\Storage\Common\Models\SharedAccessSignatureHelper;
 
 class Docente extends Model
 {
@@ -21,27 +20,18 @@ class Docente extends Model
     public function estadoCivil(){
         return $this->hasOne(EstadoCivil::class, 'idEstadoCivil', 'idEstadoCivil');
     }
-
+    
     public function getFeaturedSasUrlAttribute()
     {
         if (!$this->featured) {
             return null;
         }
-
+    
         $accountName = env('AZURE_STORAGE_NAME');
-        $accountKey = env('AZURE_STORAGE_KEY');
         $container = env('AZURE_STORAGE_CONTAINER');
-
-        $sasHelper = new SharedAccessSignatureHelper($accountName, $accountKey);
-
-        $token = $sasHelper->generateBlobServiceSharedAccessSignatureToken(
-            'b',
-            "$container/{$this->featured}",
-            'r',
-            now()->format('Y-m-d\TH:i:s\Z'),
-            now()->addMinutes(15)->format('Y-m-d\TH:i:s\Z')
-        );
-
-        return "https://{$accountName}.blob.core.windows.net/{$container}/{$this->featured}?{$token}";
+        $sasToken = env('AZURE_STORAGE_SAS_TOKEN');
+    
+        return "https://{$accountName}.blob.core.windows.net/{$container}/{$this->featured}?{$sasToken}";
     }
+    
 }
